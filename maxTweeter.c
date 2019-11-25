6f3efc5d719332a5b0db6e794c5f040fbac7cede
HiDoYa <hiroyagojo@gmail.com>
2019-11-25 01:25:51 -0800

    Added linkedlist and parsing

--- maxTweeter.c
+++ maxTweeter.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LINE_MAX 1025
+#define LINE_MAX 1024
+
+struct linkedlist {
+	struct node* head;
+	struct node* tail;
+}
 
 struct node {
 	char* name;
@@ -10,20 +15,88 @@ struct node {
 	struct node* next;
 }
 
-// Returns -1 if invalid line
-// Otherwise, counts the name
-int parse_line(char* line) {
-	// TODO Complete this
+void linkedlist_add(struct linkedlist* list, char* str) {
+	struct node* new_node = malloc(sizeof(struct node));
+	new_node->name = malloc(strlen(str));
+	strcpy(new_node->name, str);
+	new_node->count = 1;
+	new_node->next = NULL;
+
+	if (linkedlist->head == NULL) {
+		// If no other nodes exist
+		linkedlist->head = new_node;
+		linkedlist->tail = new_node;
+	} else {
+		// If other nodes exist
+		linkedlist->tail->next = new_node;
+		linkedlist->tail = new_node;
+	}
 }
 
+void linkedlist_inc(struct linkedlist* list, char* str) {
+	struct node* current = list->head;
+	while (current != NULL) {
+		if (strcmp(current->name, str) == 0) {
+			current->count++;
+			return;
+		}
+
+		current = current->next;
+	}
+
+	// If arrives here, need to add to linkedlist
+	linkedlist_add(list, str);
+}
+
+void linkedlist_del(struct linkedlist* list) {
+	struct node* current = list->head;
+	while (current != NULL) {
+		struct node* temp = current->next;
+		free(current->name);
+		free(current);
+		current = temp;
+	}
+
+
+	free(list);
+}
+
+int find_next_comma(char* str) {
+	for (int i = 0; i < LINE_MAX; i++) {
+		if (line[i] == '\0') return -1;
+		if (line[i] == ',') return i;
+	}
+	return -1;
+}
+
+// Returns -1 if invalid line
+// Otherwise, counts the name
+// TODO Check if this works
+// TODO Have to account for quotations around the word
+int parse_line(char* line, struct linkedlist* list, int name_loc) {
+	char buf[LINE_MAX];
+	int comma_count = 0;
+	for (int i = 0; i < LINE_MAX; i++) {
+		if (line[i] == '\0') return -1;
+		if (comma_count == name_loc) {
+			int len = find_next_comma(line + i);
+			if (len == -1) return -1;
+			strncpy(buf, line + i, len);
+			linkedlist_inc(list, buf);
+		}
+		if (line[i] == ',') comma_count++;
+	}
+}
+
+// TODO Need to ensure header is valid
 int get_name_location(char* header) {
 	// Must have "name" column
 	int comma_count = 0;
-	const int NAME_SIZE = 6;
 	for (int i = 0; i < LINE_MAX - NAME_SIZE; i++) {
 		if (header[i] == '\0') return -1;
 		if (header[i] == ',') comma_count++;
-		if (strncmp(header + i, "\"name\"", NAME_SIZE)) return comma_count;
+		if (strncmp(header + i, "name", 4)) return comma_count;
+		if (strncmp(header + i, "\"name\"", 6)) return comma_count;
 	}
 	return -1;
 }
@@ -43,25 +116,25 @@ int main(int argc, char** argv) {
 	// Parse header
 	char line[LINE_MAX];
 	fgets(line, LINE_MAX, csv);
-	int name_location = get_name_location(line);
-	if (name_location == -1) {
+	int name_loc = get_name_location(line);
+	if (name_loc == -1) {
 		printf("Invalid Input Format\n");
 		return -1;
 	}
 
-	// TODO Store in some data structure (linked list for now)
-	struct node* head = malloc(sizeof(struct node));
+	// Store in some data structure (linked list for now)
+	struct linkedlist* list = malloc(sizeof(struct linkedlist));
 
-	// TODO Save here
 	while (getc(csv) != EOF) {
 		fgets(line, LINE_MAX, csv);
-		if (parse_line(line) == -1) {
+
+		if (parse_line(line, name_loc, list) == -1) {
 			printf("Invalid Input Format\n");
 			return -1;
 		}
 	}
 
-	// TODO Free memory
+	linkedlist_del(list);
 
 	return 0;
 }
